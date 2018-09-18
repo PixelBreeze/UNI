53134c81086e54a689aa8d7af542f6a2123486e1
PixelBreeze <thepixelbreeze@gmail.com>
2018-09-18 22:43:09 +0300

    Update 1ldcpp.cpp

--- 1ldcpp.cpp
+++ 1ldcpp.cpp
@@ -32,7 +32,7 @@ int main()
     bool run;
     double a, b, result;
     std::cout << std::fixed;
-    std::cout << std::setprecision(3);
+    std::cout << std::setprecision(3);//set decimal point 3 0's
     run = true;
     char choice;
     while (run == true) {
@@ -46,15 +46,15 @@ int main()
             cout << "Nevar vilkt kvadratsakni no - skaitla."<< endl;
             continue;
         }
-        if (b > 10) {
+        if (b > 10) { //if C true f1 else f2
             result = 3 * pow(b, 2) - pow(a, 4);
-            std::cout << "f1 y: " << result << endl;
+            std::cout << "f1 y: " << result << endl;//f1 result
         }
         else {
             result = (a + b) / 2;
-            std::cout << "f2 y: " << result << endl;
+            std::cout << "f2 y: " << result << endl;//f2 result
         }
-        //continue program
+        //continue task?
         cout << "Vai atkartot uzdevumu?(Y/N)" << endl;
         cin >> choice;
         if (choice == 'N' || choice == 'n') {
