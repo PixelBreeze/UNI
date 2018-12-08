8790900ae02b349b6bbe49a661362a3adf5bc7a0
PixelBreeze <thepixelbreeze@gmail.com>
2018-12-08 16:53:34 +0200

    done

--- ld2var1.cpp
+++ ld2var1.cpp
@@ -1,178 +1,312 @@
-#include<iostream>
-#include<iomanip>
-#include<limits>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<getopt.h>
-#include<new>
+// reading a text file
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <regex>
+#include <Windows.h>
 
-int getNumber()
-{
-    int x;
-    while(!(std::cin >> x)){
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Wrong Input. Try Again: ";
-    }
-    return x;
+using namespace std;
+
+void moveConsole(){
+    for(int i=0; i<11; i++)
+        std::cout<<std::endl;
 }
 
-float getArrVal()
+void pressKey(){
+    std::cout << "Press ENTER to continue...";
+    std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
+    std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
+}
+//remove spaces in line
+string delSpaces(std::string line)
 {
-    float x;
-    while(!(std::cin >> x)){
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Wrong Input. Try Again: ";
-    }
-    return x;
+    line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+    return line;
 }
 
-void outputArray(int rows,int columns,float array[])
-{
-    int i,j;
-    for (i = 0; i < rows; i++){
-        for (j = 0; j < columns; j++)
-        {
-			std::cout << std::setw(8) << std::left << array[i*columns+j];
+void findString(string array[][5],string input,int c,int pos){
+    bool found = false;
+    for( int i=0 ; i<c ; i++ ) {
+        if (input.compare(array[i][pos]) == 0) {
+            for( int j=0 ; j<5 ; j++ ) {
+                std::cout<<array[i][j]<<" ";
+            }
+            found = true;
+            std::cout<<std::endl;
+        }
+    }
+    if(! found) {
+        std::cout<<"Students pēc šāda uzvārda netika atrasts!"<<std::endl;
+    } else {
+        std::cout<<std::endl;
+    }
+
+}
+
+void outputArray(string array[][5],int row) {
+    std::string header[5]={"Vārds","Uzvārds","Dzimšanas Dat.","Apliecības Nr.","Vidējā Atzīme"};
+    for (int k=0; k<5; k++) {
+        std::cout<<"* "<<std::setw(19)<< std::left<<header[k];
+    }
+    std::cout<<std::endl;
+    for( int i=0 ; i<row ; i++ ) {
+        for( int j=0 ; j<5 ; j++ ){
+            std::cout<<"| "<<std::setw(18) << std::left <<array[i][j];
         }
         std::cout<<std::endl;
     }
 }
 
-float* ArrayManual(int size)
-{
-    float* arrManual = new float[size];
-    int i;
-    std::cout << "Manual number input!" << std::endl;
-    for (i = 0; i < size; i++){
-            arrManual[i] = getArrVal();
-        }
-    return arrManual;
-}
-
-float* ArrayRand(int size)
-{
-    float* arrRand = new float[size];
-    int i;
-    std::cout << "Parameter --rand found, numbers will be random!" << std::endl;
-	float Max = 50;
-	float Min = -50;
-		for (i = 0; i < size; i++){
-            arrRand[i] = ((float(rand()) / float(RAND_MAX)) * (Max - Min)) + Min;
-        }
-    return arrRand;
-}
-
-void variants(float array[], int rows,int columns)
-{
-    float Svalue;
-    float *arrVariants = new float[9];
-    int *skipOverArr = new int[3];
-    bool skipOver=false;
-    int i,j,k=0,z=0,m,Scol,Srow,Spos;
-    skipOverArr[0] = -1;
-    for (m = 0; m < 3; m++)// go through everything 3x
-    {
-        Svalue = 99999;
-        for (i = 0; i < rows; i++) //array go
-        {
-            for (j = 0; j < columns; j++)//array go
-            {
-                if (array[i*columns+j] <= Svalue) //check if current element is smaller than previous smallest
-                {
-                    //add check if any of the positions were used already
-                    for(int n = 0; n < m; n++) {
-                        if(i*columns+j == skipOverArr[n]) {
-                            skipOver = true;
-                        }
-                    }
-                    if (skipOver == false) {
-                        Svalue = array[i*columns+j];
-                        Srow = i+1;
-                        Scol = j+1;
-                        Spos = i*columns+j;
-                        //std::cout << "New small value:" << Svalue << " " << Srow << Scol << std::endl;
-                    }
-                    skipOver=false;
-                }
+//create sorted array and pass to output.
+void sortArray(string array[][5],int rows) {
+    std::string sortedArray[rows][5];
+    int currentRow=0;
+    double smallest=11;
+    for (int i = 0; i < rows; i++) {
+        for(int j = 0; j < rows; j++) {
+            double current = ::atof(array[j][4].c_str());
+            if(smallest >= current && current <= 10) {
+                smallest = current;
+                currentRow = j;
             }
         }
-        skipOverArr[m] = Spos;
-        arrVariants[k] = Svalue;
-        arrVariants[k+1] = Srow;
-        arrVariants[k+2] = Scol;
-        std::cout << arrVariants[k] << " row: " << arrVariants[k+1] << " column: " << arrVariants[k+2] << std::endl;
-        k=k+3;
-    }
-}
-
-int ProcessArgs(int argc, char** argv)
-{
-    const char* const short_opts = "r";
-    const option long_opts[] = {
-            {"rand", no_argument, nullptr, 'r'},
-            {nullptr, no_argument, nullptr, 0}
-    };
-    while (true)
-    {
-        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
-        if (-1 == opt) {
-            break;
+        for(int k = 0; k < 5; k++) {
+            sortedArray[i][k] = array[currentRow][k];
         }
-        switch (opt)
-        {
-        case 'r':
-            return 1;
-            break;
-        case '?': // Unrecognized option
-            std::cout << "Unrecognised parameter!\n--rand/--r For random numbers.\n<none> For manual input.\n";
-            return 2;
-            break;
-        default:
-            break;
+        smallest = 11;
+        array[currentRow][4]="12";
+    }
+    for( int i=0 ; i<rows ; i++ ) {
+        for( int j=0 ; j<5 ; j++ ){
+            array[i][j]=sortedArray[i][j];
         }
     }
+    std::cout<<"\nDati tika sakāroti!\n";
+    pressKey();
 }
 
-int main(int argc, char **argv)
-{
-    std::cout << std::fixed;
-    std::cout << std::setprecision(2);//set decimal point 2 0's
-    int x;
-    int rows,columns;
-    x=ProcessArgs(argc, argv);
-    if (x==2) {
-        std::cout << "Exiting!";
-        return 0;
+//validates each item from line
+bool processItem(string item, int c) {
+    bool result = false,hasNumb=false; //must be false
+    switch(c) {
+        //name
+        case 0 :
+            //std::cout << "name: ";
+            hasNumb = std::any_of(item.begin(), item.end(), ::isdigit);
+            if (! hasNumb) {
+                result = true;
+            }
+            break;
+            //surname
+        case 1 :
+            //std::cout << "surname: ";
+            hasNumb = std::any_of(item.begin(), item.end(), ::isdigit);
+            if (! hasNumb) {
+                result = true;
+            }
+            break;
+            //DoB
+        case 2 : {
+            //std::cout << "DoB: ";
+            int c=0,numbItem=0;
+            std::string subItem;
+            std::regex regexPattern ("\\d{2}\\.\\d{2}\\.\\d{4}\\.");
+            if (std::regex_match (item, regexPattern)) {
+                //std::cout << "date matched\n";
+                std::string delimiter = ".";
+                size_t pos = 0;
+                while ((pos = item.find(delimiter)) != std::string::npos) {
+                    subItem = item.substr(0, pos);
+                    numbItem = std::stoi(subItem);
+                    switch(c) {
+                        case 0 :
+                            if (numbItem > 31 || numbItem < 1) {
+                                return false;
+                            }
+                            break;
+                        case 1 :
+                            if (numbItem > 12 || numbItem < 1) {
+                                return false;
+                            }
+                            break;
+                        case 2 :
+                            if (numbItem > 2018 || numbItem < 1500) {
+                                return false;
+                            }
+                            break;
+                    }
+                    item.erase(0, pos + delimiter.length());
+                    c++;
+                }
+                result = true;
+            }
+            break;
+        }
+            //ID
+        case 3 : {
+            //std::cout << "ID: ";
+            std::regex regexPattern ("^[0-9]{3}[A-Z]{3}[0-9]{3}$");
+            if (std::regex_match (item, regexPattern)) {
+                //std::cout << "ID Matched\n";
+                return true;
+            }
+            break;
+        }
+            //mark
+        case 4 :{
+            //std::cout << "avgMark: ";
+            std::regex regexPattern ("^\\d{1,2}\\.\\d{2}$");
+            if (std::regex_match (item, regexPattern)) {
+                //std::cout << "Mark Matched\n";
+                double mark = ::atof(item.c_str());
+                if (mark >= 0 && mark <= 10 ) {
+                    return true;
+                }
+            }
+            break;
+        }
     }
-    std::cout << "Enter Row count for array: ";
-    rows=getNumber();
-    while (rows <= 0) {
-        std::cout << "Wrong Input. Try Again: ";
-        rows=getNumber();
+    return result;
+}
+
+//validate the length based on delimiter count and check if not empty tag
+bool processLine(std::string line) {
+    int lngth = 0;
+    string item;
+    //split string
+    std::string delimiter = ";";
+    size_t pos = 0;
+    while ((pos = line.find(delimiter)) != std::string::npos) {
+        item = line.substr(0, pos);
+        line.erase(0, pos + delimiter.length());
+        if (item.length() != 0) {
+            lngth++;
+        }
     }
-    std::cout << "Enter Column count for array: ";
-    columns=getNumber();
-    while (columns <= 0) {
-        std::cout << "Wrong Input. Try Again: ";
-        columns=getNumber();
+    //std::cout << lngth <<std::endl;
+    if (lngth == 5) { return true;}
+    else { return false; }
+}
+
+int main () {
+    SetConsoleOutputCP(CP_UTF8);
+    setvbuf(stdout, nullptr, _IOFBF, 1000);
+    std::cout<<u8"171RDB271 Ernests Šutko 2.grupa"<<std::endl;
+    //variables
+    string line,fline,item;
+    string filename;
+    ofstream errFile;
+    errFile.open ("err.txt");
+    bool fileOpen=false,valid=false,validItem=false,failed=false,run=true;
+    int col=0,row=0;
+    //2d array supports 100 lines
+    string lineData [100][5];
+    //open file. while file not open try again.
+    while(! fileOpen) {
+        std::cout << "Ievadiet faila nosaukumu: ";
+        std::cin >> filename;
+        ifstream myfile(filename);
+        if (myfile.is_open()) {
+            fileOpen = true;
+        }
+        else cout << "Nebija iespējams atvērt failu vai fails netika atrasts.\nMeiģiniet velreiz." << std::endl;
     }
-    int size=rows*columns;//define array size
-    float* array;
-    //create new array
-    if(x==-1) {
-        array=ArrayManual(size);
-    } else {
-        array=ArrayRand(size);
+    //readfile
+    ifstream dataFile(filename);
+    while ( std::getline(dataFile, line)) {
+        fline = delSpaces(line);
+        if (fline == "") continue;
+        //cout << line << '\n';
+        valid = processLine(fline);
+        //if there are exact number of delimiters = 5
+        if (valid) {
+            //split string
+            std::string delimiter = ";";
+            size_t pos = 0;
+            //cout << "ROW: " << row << '\n';
+            while ((pos = fline.find(delimiter)) != std::string::npos) {
+                item = fline.substr(0, pos);
+                //cout << "current column: " << col << " item is: " << item << '\n';
+                validItem = processItem(item, col);
+                if (validItem) {
+                    //insert formatted item string into array of lines
+                    string fItem = delSpaces(item);
+                    lineData[row][col] = fItem;
+                    col++;
+                } else {
+                    col=0;
+                    failed = true;
+                    //insert whole line into err.txt and don't move rows, set col 0, exit while
+                    break;
+                }
+                fline.erase(0, pos + delimiter.length());
+            }
+            if (! failed) {
+                col = 0;
+                row++;
+            } else {
+                fline = delSpaces(line);
+                errFile << fline <<"\n";
+            }
+            failed = false;
+        } else {
+            fline = delSpaces(line);
+            errFile << fline <<"\n";
+        }
     }
-    std::cout << std::endl;
-    outputArray(rows,columns,array);
-    //varianta uzd
-    std::cout << std::endl;
-    std::cout << "1. Varianta uzdevuma masivs: " << std::endl;
-    variants(array,rows,columns);
-    delete[] array;
+    std::cout << "\nPareizas līnijas apstrādātas: " <<row<<"\n\n";
+    outputArray(lineData,row);
+    dataFile.close();
+    errFile.close();
+    //function calls for tasks a,b,c
+    while(run) {
+        string inp;
+        std::cout<<"\n1. Atrast studentu, kura apliecības numurs atbilst lietotāja ievadītajam."
+        <<"\n2. Meklēt studentu pēc uzvārda."
+        <<"\n3. Sakārtot datus par studentiem augošā secībā pēc vidējās atzīmes."
+        <<"\n4. Iziet."<<std::endl;
+        std::cout<<"\nIzvēlieties darbības nr: ";
+        std::cin>>inp;
+        bool hasNumb = std::any_of(inp.begin(), inp.end(), ::isdigit);
+        if (!hasNumb) {
+            inp = "0";
+        }
+        int task = std::stoi(inp);
+        switch(task) {
+            case 1: {
+                std::cout<<"\nIevadiet studenta apliecības nr: ";
+                string id;
+                std::cin>>id;
+                findString(lineData, id,row,3);
+                pressKey();
+                break;
+            }
+            case 2: {
+                std::cout << "\nIevadiet studenta uzvārdu: ";
+                string surname;
+                std::cin >> surname;
+                findString(lineData, surname, row, 1);
+                pressKey();
+                break;
+            }
+            case 3:
+                sortArray(lineData,row);
+                break;
+            case 4: {
+                run = false;
+                break;
+            }
+            default:
+                std::cout<<"Šādas darbības nav!"<<std::endl;
+                pressKey();
+                break;
+        }
+        if (run)  {
+            moveConsole();
+            outputArray(lineData,row);
+        }
+    }
     return 0;
 }
